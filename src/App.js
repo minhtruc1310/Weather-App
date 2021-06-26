92ec821dc44aa0eab3bd71a1703a3eaeb60f8fb7
Minh Truc <66997070+minhtruc1310@users.noreply.github.com>
2021-06-26 17:30:38 +0700

    2nd update

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
                     <div className="date">{dateBuilder(new Date())}</div>
                 </div>
                 <div className="weather-box">
-                    <div className="temp">{Math.round(weather.main.temp)}</div>
+                    <div className="temp">{Math.round(weather.main.temp)}°C</div>
                     <div className="weather-cur">{weather.weather[0].main}</div>
                 </div>
             </div>
